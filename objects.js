8001861f4ffd7507f21445ad562fba16cc6851bc
JayasreePrabhakar <jayasreeprabhakar06@gmail.com>
2025-06-19 16:19:38 +0530

    complete spread topic

--- objects.js
+++ objects.js
@@ -14,3 +14,37 @@ let newObject = {
 console.log(newObject.name);
 console.log(newObject.age);
 newObject.func(2, 3);
+
+
+//De-structuring:
+function getDetails() {
+    let person = {
+        name: "Jayasree",
+        age: 25,
+    };
+    return person;
+}
+
+//let returnValue = getDetails();//manually done
+//let name = returnValue.name;//no need
+//let age = returnValue.age;//no need
+
+//et { newName, newAge } = getDetails();
+
+
+//using spread operators
+let person = {
+     name: "Jayasree",
+     age: 25,
+};
+
+let newDetails ={
+    ...person,
+    role: "developer",
+    company: "ABC Pvt Ltd",
+    isMarried: false,
+    salary: 2.5,
+};
+
+console.log(newDetails);
+
