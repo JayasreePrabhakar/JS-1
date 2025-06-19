033ac9ba412e5fb9a207bf906cb7261448accbed
JayasreePrabhakar <jayasreeprabhakar06@gmail.com>
2025-06-19 10:02:31 +0530

    complete data-types topic

---
+++ data-types.js
@@ -0,0 +1,9 @@
+let name = "Jayasree";
+let age = 19;
+let price = 100.25;
+let isStudent = "True";
+
+console.log(name);
+console.log(age);
+console.log(price);
+console.log(isStudent);
---
+++ typing.js
@@ -0,0 +1,3 @@
+console.log("Hello Jayasree, How are you?");
+console.log("2" + 2);
+console.log("2" - 2);
