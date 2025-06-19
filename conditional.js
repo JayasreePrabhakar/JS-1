b26b447bc26d266442c4a0cadb7a456719c42730
JayasreePrabhakar <jayasreeprabhakar06@gmail.com>
2025-06-19 11:21:42 +0530

    complete conditionals topic

---
+++ conditional.js
@@ -0,0 +1,14 @@
+let marks = 85;
+if(marks >= 85) {
+    console.log("A grade");
+}else if(marks >= 75){
+    console.log("B grade");
+}else{
+    console.log("C grade");
+}
+
+if(0){
+    console.log("Working");
+}else{
+    console.log("False");
+}
